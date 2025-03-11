8f12b47a40de27d5ee715ca09aef90606273904d
Shibam Mohanty <106046613+SHIBAMMOHANTY@users.noreply.github.com>
2025-03-11 16:15:35 +0530

    push

--- BHOOMIKA/bhoomika/src/components/Navbar.jsx
+++ BHOOMIKA/bhoomika/src/components/Navbar.jsx
@@ -10,12 +10,12 @@ const Navbar = () => {
             <img className='w-22 cursor-pointer' src={bhoomika} alt="" />
             <div className='flex items-center justify-between gap-8'>
               <ul className='hidden lg:flex flex-row lg:gap-8 font-semibold'>
-                <li className='cursor-pointer md:hover:scale-105 lg:hover:scale-120 transition-all duration-300 ease-in-out hover:text-lime-500'>Home</li>
-                <li className='cursor-pointer md:hover:scale-105 lg:hover:scale-120 transition-all duration-300 ease-in-out hover:text-lime-500'>About</li>
-                <li className='cursor-pointer md:hover:scale-105 lg:hover:scale-120 transition-all duration-300 ease-in-out hover:text-lime-500'>Services</li>
-                <li className='cursor-pointer md:hover:scale-105 lg:hover:scale-120 transition-all duration-300 ease-in-out hover:text-lime-500'>Blogs</li>
-                <li className='cursor-pointer md:hover:scale-105 lg:hover:scale-120 transition-all duration-300 ease-in-out hover:text-lime-500'>Career</li>
-                <li className='cursor-pointer md:hover:scale-105 lg:hover:scale-120 transition-all duration-300 ease-in-out hover:text-lime-500'>Contact</li>
+               <a href="/"><li className='cursor-pointer md:hover:scale-105 lg:hover:scale-120 transition-all duration-300 ease-in-out hover:text-lime-500'>Home</li></a> 
+              <a href=""><li className='cursor-pointer md:hover:scale-105 lg:hover:scale-120 transition-all duration-300 ease-in-out hover:text-lime-500'>About</li></a>  
+               <a href="/service"><li className='cursor-pointer md:hover:scale-105 lg:hover:scale-120 transition-all duration-300 ease-in-out hover:text-lime-500'>Services</li></a> 
+               <a href=""><li className='cursor-pointer md:hover:scale-105 lg:hover:scale-120 transition-all duration-300 ease-in-out hover:text-lime-500'>Blogs</li></a> 
+               <a href=""><li className='cursor-pointer md:hover:scale-105 lg:hover:scale-120 transition-all duration-300 ease-in-out hover:text-lime-500'>Career</li></a> 
+                <a href="/contact"><li className='cursor-pointer md:hover:scale-105 lg:hover:scale-120 transition-all duration-300 ease-in-out hover:text-lime-500'>Contact</li></a>
               </ul>
               <button type="button" class="hidden lg:inline-flex text-gray-900 hover:text-white bg-lime-400 hover:bg-gradient-to-r from-[#04637B] via-cyan-600 to-[#04637B] font-medium rounded-lg text-sm px-4 py-2.5 text-center items-center transition-all duration-300 ease-in-out transform hover:scale-105 active:scale-95 shadow-md hover:shadow-xl cursor-pointer">
                 Book an Appointment
