c75918d8a458fbe172124b3239aa31da2b23683e
Abhishekhansdak <abhishekhansdak53@gmail.com>
2025-03-11 09:58:45 +0530

    upadte

---
+++ BHOOMIKA/bhoomika/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/navbar';
+import Hero from './components/Hero';
+import Ads from './components/Ads';
+
+const App = () => {
+  return (
+    <>
+      <Ads />
+      <Navbar />
+      <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ BHOOMIKA/bhoomika/src/components/Ads.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Ads = () => {
+  return (
+    <>
+    <div>
+    <div className='hidden sm:flex sm:justify-between sm:items-center relative sm:w-full h-7.5 sm:bg-linear-to-r from-blue-500 via-cyan-500 to-teal-500'>
+        <p className='sm:px-2 sm:mx-10 lg:px-10 lg:mx-30 font-semibold text-white'>🗞️ info@bhoomikaeyeinstitute</p>
+        <p className='sm:px-2 sm:mx-10 lg:px-10 lg:mx-30 font-semibold text-white'>📌 +91 9777050048</p>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Ads
